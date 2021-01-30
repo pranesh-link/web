ad7eceffca22713e8ee1e05c15d80679a1455c40
Pranesh <prans1991@gmail.com>
2021-01-31 00:34:04 +0530

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import MenuBar from "./components/MenuBar";
 import ProfileSections from "./components/ProfileSections";
