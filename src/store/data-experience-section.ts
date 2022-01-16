aaa8bda44b5dca49572babd0161c0477b8e3f677
Pranesh G <pg@ciena.com>
2022-01-16 16:38:32 +0530

    Add Brillio experience

--- src/store/data-experience-section.ts
+++ src/store/data-experience-section.ts
@@ -5,6 +5,38 @@ const Experiences: ISectionInfo = {
   ref: "experienceRef",
   info: [
     {
+      organization: "Brillio Technologies",
+      projects: [
+        {
+          title: { info: "MCP" },
+          client: { info: "Ciena" },
+          duration: { info: "Feb 2021-" },
+          softwareTech: { info: "EmberJS" },
+          description: {
+            info: `Manage, Control and Plan (MCP) software converges the complete network operations lifecycle through network planning, 
+                   infrastructure commissioning, service fulfillment, and service assurance. MCP converges network and service management 
+                   throughout the complete lifecycle. It enables carriers and enterprises to take advantage of the following:
+                   <ul>
+                       <li>Lower operating expenditures</li>
+                       <li>Lower capital expenditure</li>
+                       <li>Increased revenues and ROI</li>
+                   </ul>`,
+          },
+          responsibilities: {
+            info: `
+                <ul>
+                    <li>Development of web UI</li>.
+                    <li>Unit test and integration tests</li>
+                    <li>Code review with fellow colleagues</li>
+                    <li>Educating team towards better coding standards</li>
+                </ul>
+              `,
+            requiresShowHide: true,
+          },
+        },
+      ],
+    },
+    {
       organization: "Standard Chartered GBS",
       projects: [
         {
