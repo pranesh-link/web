8bbb470b3828d208a177f76267be475310c25174
Pranesh <prans1991@gmail.com>
2022-12-20 22:23:43 +0530

    Remove duplicate Constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -52,3 +52,14 @@ export const DEFAULT_CONTEXT = {
   isDownloading: false,
   isMobile: false,
 };
+
+export const LABEL_TEXT: { [key: string]: string } = {
+  client: "Client",
+  duration: "Duration",
+  description: "Description",
+  responsibilities: "Responsibilities",
+  softwareTech: "Software/Technologies",
+};
+
+export const SHORT_INFOS = ["client", "duration", "softwareTech"];
+export const EXPANDABLE_INFOS = ["description", "responsibilities"];
