97dbf837e0e6d7184c90389444e1dde210d6375e
Pranesh <prans1991@gmail.com>
2022-12-20 22:40:35 +0530

    Remove store files and logo images

--- src/store/data-education-section.ts
+++
@@ -1,8 +0,0 @@
-import { ISectionInfo } from "./types";
-
-const Education: ISectionInfo = {
-  title: "Education",
-  ref: "educationRef",
-  info: "<strong>B.E (Electronics and Communication)</strong>, Sri Krishna College of Engineering and Technology, Coimbatore",
-};
-export default Education;
