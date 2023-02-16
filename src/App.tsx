4e158cd5ed1de505fbafb0ccffee801628752916
Pranesh <prans1991@gmail.com>
2023-02-16 10:19:29 +0530

    Add common data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,16 @@ import {
 import { ISectionInfo } from "./store/profile/types";
 import { LoaderImg } from "./common/Elements";
 import LoaderIcon from "./assets/loader-icon.svg";
+import { CommonDataType } from "./store/common/types";
 
 function App() {
   const queryParams = new URLSearchParams(window.location.search);
   const isAdmin = queryParams.get("admin");
+  const isExport = !!queryParams.get("export");
   const [maintenance, setMaintenance] = useState(DEFAULT_MAINTENANCE_DATA);
   const [links, setLinks] = useState(DEFAULT_CONTEXT.data.sections.links);
   const [pwa, setPwa] = useState(DEFAULT_PWA);
+  const [commonData, setCommonData] = useState<CommonDataType>({});
   const [hasError, setHasError] = useState(false);
 
   const fetchSections = async (jsonToFetch: string, data: ISectionInfo) => {
@@ -33,14 +36,17 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const [maintenanceInfo, linksInfo, pwaInfo] = await Promise.all([
-        fetchData("maintenance"),
-        fetchSections("links", links),
-        fetchData("pwa"),
-      ]);
+      const [maintenanceInfo, linksInfo, pwaInfo, commonData] =
+        await Promise.all([
+          fetchData("maintenance"),
+          fetchSections("links", links),
+          fetchData("pwa"),
+          fetchData("common"),
+        ]);
       setMaintenance(maintenanceInfo);
       setLinks(linksInfo);
       setPwa(pwaInfo);
+      setCommonData(commonData);
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -80,7 +86,12 @@ function App() {
                   path="/profile"
                   element={
                     <>
-                      <ProfilePageComponent hasError={hasError} pwa={pwa} />
+                      <ProfilePageComponent
+                        isExport={isExport}
+                        hasError={hasError}
+                        pwa={pwa}
+                        commonData={commonData}
+                      />
                     </>
                   }
                 />
