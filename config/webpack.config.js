d2409ec58e213b807400d184826393a7dfca1dd1
Pranesh <prans1991@gmail.com>
2023-02-16 10:20:00 +0530

    Emit build error for production builds

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -714,7 +714,7 @@ module.exports = function (webpackEnv) {
         }),
       new ESLintPlugin({
         // Plugin options
-        emitError: true,
+        emitError: isEnvProduction,
         extensions: ["js", "mjs", "jsx", "ts", "tsx"],
         formatter: require.resolve("react-dev-utils/eslintFormatter"),
         eslintPath: require.resolve("eslint"),
